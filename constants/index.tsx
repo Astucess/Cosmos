9d5dba482961a6e2b847580f2f5f248e7b1a0ee9
Astucess <leonis3ild@gmail.com>
2025-09-06 13:15:16 +0530

    Update index.tsx

--- constants/index.tsx
+++ constants/index.tsx
@@ -7,254 +7,36 @@ import {
   RxLinkedinLogo,
 } from "react-icons/rx";
 
-export const SKILL_DATA = [
-  {
-    skill_name: "HTML",
-    image: "html.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "CSS",
-    image: "css.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "JavaScript",
-    image: "js.png",
-    width: 65,
-    height: 65,
-  },
-  {
-    skill_name: "Tailwind CSS",
-    image: "tailwind.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "React",
-    image: "react.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Redux",
-    image: "redux.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "React Query",
-    image: "reactquery.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "TypeScript",
-    image: "ts.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Next.js 14",
-    image: "next.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Framer Motion",
-    image: "framer.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Stripe",
-    image: "stripe.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Node.js",
-    image: "node.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "MongoDB",
-    image: "mongodb.png",
-    width: 40,
-    height: 40,
-  },
-] as const;
+export const LINKS = {
+  sourceCode: "https://github.com/astucess",
+};
+
+export const NAV_LINKS = [
+  { title: "About me", link: "#about-me" },
+  { title: "Skills", link: "#skills" },
+  { title: "Projects", link: "#projects" },
+];
 
 export const SOCIALS = [
-  {
-    name: "Instagram",
-    icon: RxInstagramLogo,
-    link: "https://instagram.com",
-  },
-  {
-    name: "Facebook",
-    icon: FaFacebook,
-    link: "https://facebook.com",
-  },
-  {
-    name: "Twitter",
-    icon: RxTwitterLogo,
-    link: "https://twitter.com",
-  },
-] as const;
+  { name: "Instagram", icon: RxInstagramLogo, link: "https://instagram.com" },
+  { name: "Facebook", icon: FaFacebook, link: "https://facebook.com" },
+  { name: "Twitter", icon: RxTwitterLogo, link: "https://twitter.com" },
+];
 
-export const FRONTEND_SKILL = [
-  {
-    skill_name: "HTML",
-    image: "html.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "CSS",
-    image: "css.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "JavaScript",
-    image: "js.png",
-    width: 65,
-    height: 65,
-  },
-  {
-    skill_name: "Tailwind CSS",
-    image: "tailwind.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Material UI",
-    image: "mui.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "React",
-    image: "react.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Redux",
-    image: "redux.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "React Query",
-    image: "reactquery.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "TypeScript",
-    image: "ts.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Next.js 14",
-    image: "next.png",
-    width: 80,
-    height: 80,
-  },
-] as const;
-
-export const BACKEND_SKILL = [
-  {
-    skill_name: "Node.js",
-    image: "node.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Express.js",
-    image: "express.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "MongoDB",
-    image: "mongodb.png",
-    width: 40,
-    height: 40,
-  },
-  {
-    skill_name: "Firebase",
-    image: "firebase.png",
-    width: 55,
-    height: 55,
-  },
-  {
-    skill_name: "PostgreSQL",
-    image: "postgresql.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "MySQL",
-    image: "mysql.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "Prisma",
-    image: "prisma.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "Graphql",
-    image: "graphql.png",
-    width: 80,
-    height: 80,
-  },
-] as const;
-
-export const FULLSTACK_SKILL = [
-  {
-    skill_name: "React Native",
-    image: "reactnative.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "Tauri",
-    image: "tauri.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "Docker",
-    image: "docker.png",
-    width: 70,
-    height: 70,
-  },
-
-  {
-    skill_name: "Figma",
-    image: "figma.png",
-    width: 50,
-    height: 50,
-  },
-] as const;
-
-export const OTHER_SKILL = [
-  {
-    skill_name: "Go",
-    image: "go.png",
-    width: 60,
-    height: 60,
-  },
+export const SKILL_DATA = [
+  { skill_name: "HTML", image: "html.png", width: 80, height: 80 },
+  { skill_name: "CSS", image: "css.png", width: 80, height: 80 },
+  { skill_name: "JavaScript", image: "js.png", width: 65, height: 65 },
+  { skill_name: "Tailwind CSS", image: "tailwind.png", width: 80, height: 80 },
+  { skill_name: "React", image: "react.png", width: 80, height: 80 },
+  { skill_name: "Redux", image: "redux.png", width: 80, height: 80 },
+  { skill_name: "React Query", image: "reactquery.png", width: 80, height: 80 },
+  { skill_name: "TypeScript", image: "ts.png", width: 80, height: 80 },
+  { skill_name: "Next.js 14", image: "next.png", width: 80, height: 80 },
+  { skill_name: "Framer Motion", image: "framer.png", width: 80, height: 80 },
+  { skill_name: "Stripe", image: "stripe.png", width: 80, height: 80 },
+  { skill_name: "Node.js", image: "node.png", width: 80, height: 80 },
+  { skill_name: "MongoDB", image: "mongodb.png", width: 40, height: 40 },
 ] as const;
 
 export const PROJECTS = [
@@ -275,7 +57,7 @@ export const PROJECTS = [
   {
     title: "Portfolio",
     description:
-      "Portfolio.xyz is the experimental and creative spin on your personal showcase. It’s where you test bold layouts, futuristic animations, and interactive design ideas. , this site reflects your innovative side. It’s a playground to show the world you don’t just follow trends—you set them",
+      "Portfolio.xyz is the experimental and creative spin on your personal showcase. It’s where you test bold layouts, futuristic animations, and interactive design ideas. This site reflects your innovative side. It’s a playground to show the world you don’t just follow trends—you set them.",
     image: "/projects/project-3.png",
     link: "https://astucess.github.io/Portfolio.xyz/",
   },
@@ -294,77 +76,3 @@ export const PROJECTS = [
     link: "https://portfoliocom-nine.vercel.app/",
   },
 ] as const;
-
-
-export const FOOTER_DATA = [
-  {
-    title: "Community",
-    data: [
-      {
-        name: "YouTube",
-        icon: FaYoutube,
-        link: "https://youtube.com",
-      },
-      {
-        name: "GitHub",
-        icon: RxGithubLogo,
-        link: "https://github.com",
-      },
-      {
-        name: "Discord",
-        icon: RxDiscordLogo,
-        link: "https://discord.com",
-      },
-    ],
-  },
-  {
-    title: "Social Media",
-    data: [
-      {
-        name: "Instagram",
-        icon: RxInstagramLogo,
-        link: "https://instagram.com",
-      },
-      {
-        name: "Twitter",
-        icon: RxTwitterLogo,
-        link: "https://twitter.com",
-      },
-      {
-        name: "Linkedin",
-        icon: RxLinkedinLogo,
-        link: "https://linkedin.com",
-      },
-    ],
-  },
-  {
-    title: "About",
-    data: [
-      {
-        name: "Become Sponsor",
-        icon: null,
-        link: "https://youtube.com",
-      },
-      {
-        name: "Learning about me",
-        icon: null,
-        link: "https://example.com",
-      },
-      {
-        name: "Contact Me",
-        icon: null,
-        link: "plutowebworks@gmail.com",
-      },
-    ],
-  },
-] as const;
-
-export const NAV_LINKS = [
-  { title: "About me", link: "#about-me" },
-  { title: "Skills", link: "#skills" },
-  { title: "Projects", link: "#projects" },
-] as const;
-
-export const LINKS = {
-  sourceCode: "https://github.com/astucess",
-};
